b8553a6cfcffb6e22d02c77de2152770ce206e1e
jenil <JENIL.MONPARA@YUDIZ.COM>
2022-09-15 16:51:36 +0530

    Initial commit

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using TMPro;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager Instance;
+    public string playerName = "";
+    public int highScore;
+    public string highScorePlayer = "";
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SaveName()
+    {
+        playerName = GameObject.Find("Name Input").GetComponent<TMP_InputField>().text;
+    }
+
+    public void LoadName()
+    {
+        GameObject.Find("Name Input").GetComponent<TMP_InputField>().text = playerName;
+    }
+
+    public void LoadHighScore()
+    {
+        GameObject.Find("High Score").GetComponent<TextMeshProUGUI>().text = highScorePlayer + " currently has the high score: " + highScore;
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int highScore;
+        public string highScorePlayer;
+    }
+
+    public void SaveDetails()
+    {
+        SaveData data = new SaveData();
+        
+        data.highScore = highScore;
+        data.highScorePlayer = highScorePlayer;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/jenil.json", json);
+    }
+
+    public void LoadDetails()
+    {
+        string path = Application.persistentDataPath + "/jenil.json";
+        
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            highScore = data.highScore;
+            highScorePlayer = data.highScorePlayer;
+        }
+    }
+}
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DataManager.Instance.LoadDetails();
+        if (File.Exists(Application.persistentDataPath + "/jenil.json") == true)
+        {
+            DataManager.Instance.LoadName();
+            DataManager.Instance.LoadHighScore();
+        }
+    }
+
+    public void LoadMainScene()
+    {
+        DataManager.Instance.SaveName();
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitApplication()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
